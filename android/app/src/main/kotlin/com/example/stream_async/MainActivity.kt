1a4a40f41a75e07a622f3b2c29c954e9a2e5143f
jiwoo Yun <riudiu@pood.pet>
2022-03-03 11:20:54 +0900

    stream 비동기 처리 기본예제

---
+++ android/app/src/main/kotlin/com/example/stream_async/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stream_async
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/StreamBasic.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class StreamBasic extends StatefulWidget {
+  const StreamBasic({Key? key}) : super(key: key);
+
+  @override
+  State<StreamBasic> createState() => _StreamBasicState();
+}
+
+class _StreamBasicState extends State<StreamBasic> {
+  RxInt sum = 0.obs;
+
+  bool _pageDispose = false;
+
+  late Stream<int> _stream;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ///초기화
+                  _reset(),
+
+                  ///시작
+                  _start(),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Obx(() => Text("value : ${sum.value}",
+                  style: const TextStyle(fontSize: 20))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  ///초기화
+  Widget _reset() {
+    return ElevatedButton(
+        onPressed: () => sum.value = 0,
+        child: const Text("초기화", style: TextStyle(fontSize: 20)));
+  }
+
+  @override
+  void dispose() {
+    _pageDispose = true;
+    super.dispose();
+  }
+
+  Widget _start() {
+    return ElevatedButton(
+        onPressed: () async {
+          _stream = countStream(10);
+
+          ///take를 활용하면 take에 입력된 숫자 만큼만 listen함수로 들어온다.
+          _stream.take(9).takeWhile((element) {
+            ///takeWhile에서 false를 리턴하면 stream을 종료시킨다
+            if (element == 8) {
+              return false;
+            } else if (_pageDispose) {
+              return false;
+            } else {
+              return true;
+            }
+          }).where((event) {
+            ///where에서 false를 return하면 stream의 listener는 계속 붙어있지만
+            ///listen 함수로 진입하지는 않는다
+            return !(event > 6);
+          }).listen((v) => sum.value += v);
+        },
+        child: Container(
+            alignment: Alignment.center,
+            child: const Text("Stream basic", style: TextStyle(fontSize: 20))));
+  }
+
+  Stream<int> countStream(int to) async* {
+    for (int i = 1; i <= to; i++) {
+      await Future.delayed(const Duration(milliseconds: 300));
+      yield i;
+    }
+  }
+}
